fca9d70343f0a4ca239ab21e7556c0444f3171d0
Maximys132 <ackiypragrammist@gmail.com>
2023-08-19 19:27:59 +0300

    add first assets

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+public class BallMove : MonoBehaviour
+{
+    public float speed = 8;
+    private Rigidbody2D rg;
+    private float maxAngleInterval = 0.67f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+        Vector2 dir = Vector2.up;
+        dir.x = UnityEngine.Random.Range(-maxAngleInterval, maxAngleInterval);
+        rg.velocity = dir * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //collision.();
+        if (collision.tag == "Respawn")
+        {
+            UnityEngine.Debug.Log("ladida");
+        }
+        if (collision.tag == "Brick")
+        {
+            //collision.
+        }
+    }
+}
---
+++ Assets/Scripts/ScrBrick.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrBrick : MonoBehaviour
+{
+    private void BrackBrick()
+    {
+        UnityEngine.Debug.Log("Я ломал стекло!");
+
+        /*   Animation an = GetComponent<Animation>;
+           an.Play();*/
+    }
+}

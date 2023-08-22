973b15d78e4f0bfae0bf294ce31b73bc9249661e
Maxim Burakov <mburakov@bsw.iron>
2023-08-22 16:39:02 +0300

    New Commit 22.08.2023 16:38
    Signed-off-by: Maxim Burakov <mburakov@bsw.iron>

--- Assets/Scripts/BrickScript.cs
+++ Assets/Scripts/BrickScript.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Animator anim;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        anim.CrossFade("Shining", 0, 0, UnityEngine.Random.Range(0, 2f));
+        anim.speed = UnityEngine.Random.Range(0.5f, 1.5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate() 
+    { 
+        //if (health <= 0 )
+
+    }
+
+    [System.Obsolete]
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Ball")) 
+        {
+            anim.SetInteger("health", 0);
+            anim.speed = 2f;
+        }
+    }
+
+    void brickBreak()
+    {
+        GetComponent<Collider2D>().enabled = false;
     }
 }
---
+++ Assets/Scripts/LevelsRule.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelsRule : MonoBehaviour
+{
+    public int ballCount;
+    [SerializeField] GameObject[] bricks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

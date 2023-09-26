9d07863a01a1388967aca14ff77c55fe6b54fef8
Maximys132 <142674322+Maximys132@users.noreply.github.com>
2023-09-26 11:47:08 +0300

    add level 2 and level map
    add level 2 and level map

--- Assets/Scripts/Scr_SceneSetting.cs
+++ Assets/Scripts/Scr_SceneSetting.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 public class Scr_SceneSetting : MonoBehaviour
 {
     public GameObject PausePanel;
-    public int level;
 
     public void PauseButtonPress()
     {
